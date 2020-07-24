5d96772a4c71e64d433dfff522b1d1c1850005b0
slowdive <schlickjoe@gmail.com>
2020-07-23 18:00:39 -0700

    added quiz questions, choices, and answers.

---
+++ script.js
@@ -0,0 +1,71 @@
+// 1. What is the HTML tag under which one can write the JavaScript code?
+// A) <javascript>
+// B) <scripted>
+// C) <script>
+// D) <js>
+// Ans: C
+
+// 2. Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?
+// A. alertbox(“GeeksforGeeks”);
+// B. msg(“GeeksforGeeks”);
+// C. msgbox(“GeeksforGeeks”);
+// D. alert(“GeeksforGeeks”);
+// Ans: D
+
+// 3. What is the correct syntax for referring to an external script called “geek.js”?
+// A. <script src=”geek.js”>
+// B. <script href=”geek.js”>
+// C. <script ref=”geek.js”>
+// D. <script name=”geek.js”>
+// Ans: A
+
+// 4. Which of the following is not a reserved word in JavaScript?
+// A. interface
+// B. throws
+// C. program
+// D. short
+// Ans: C
+
+// 5. What is the syntax for creating a function in JavaScript named as Geekfunc? 
+// A) function = Geekfunc() 
+// B) function Geekfunc() 
+// C) function := Geekfunc() 
+// D) function : Geekfunc()
+// Ans: Option B 
+
+// 6. How is the function called in JavaScript?
+// A) call Geekfunc(); 
+// B) call function GeekFunc(); 
+// C) Geekfunc(); 
+// D) function Geekfunc();
+// Ans: C 
+
+// 7. How to write an ‘if’ statement for executing some code. 
+// If “i” is NOT equal to 5?
+// A) if(i<>5) 
+// B) if i<>5 
+// C) if(i!=5) 
+// D) if i!=5
+// Ans: C 
+
+// 8. What is the correct syntax for adding comments in JavaScript?
+// A) <!–This is a comment–&gt 
+// B) //This is a comment 
+// C) –This is a comment 
+// D) **This is a comment**
+// Ans: B 
+
+// // 9. What is the JavaScript syntax for printing values in Console?
+// A) print(5) 
+// B) console.log(5); 
+// C) console.print(5); 
+// D) print.console(5);
+// Ans: Option B
+
+// 10. How to initialize an array in JavaScript?
+// A) var Geeks= “Geek1”, “Geek2”, “Geek3” 
+// B) var Geeks=(1:Geek1, 2:Geek2, 3:Geek3) 
+// C) var Geeks=(1=Geek1, 2=Geek2, 3=Geek3) 
+// D) var Geeks=[“Geek1”, “Geek2”, “Geek3”]
+// Ans: D 
+
