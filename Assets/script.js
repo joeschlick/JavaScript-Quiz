c403e23930790f4438973317e533ae18becc6320
slowdive <schlickjoe@gmail.com>
2020-07-27 17:38:33 -0700

    added event listener to submit button

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,7 @@ let openingEl = document.getElementById("opening-page");
 let startBtn = document.getElementById("start-btn");
 //question and answers grabs and styling
 let questionBoxEl = document.getElementById("question-box");
-questionBoxEl.setAttribute("style","display:none");
+questionBoxEl.setAttribute("style", "display:none");
 let questionsEl = document.getElementById("questions");
 let answersEL = document.getElementById("answers");
 let questionBtnA = document.createElement("button");
@@ -24,10 +24,10 @@ answersEL.appendChild(questionBtnD);
 questionBoxEl.appendChild(rightWrongEl);
 //final page grabs
 let finalEl = document.getElementById("final-box");
-finalEl.setAttribute("style","display:none");
+finalEl.setAttribute("style", "display:none");
 let fScoreEl = document.getElementById("final-score");
 let initialsEl = document.getElementById("initials");
-let submitBtn = document.getElementById("initials");
+let submitBtn = document.getElementById("submit-btn");
 //scores.html grabs
 let scoreList = document.getElementById("scores-list");
 let goBackBtn = document.getElementById("go-back-btn");
@@ -44,12 +44,7 @@ let questions = [
   {
     question:
       "1. What is the HTML tag under which one can write the JavaScript code?",
-    choices: [
-        "A. <javascript>", 
-        "B. <scripted>", 
-        "C. <script>", 
-        "D. <js>"
-    ],
+    choices: ["A. <javascript>", "B. <scripted>", "C. <script>", "D. <js>"],
     answer: "C. <script>",
   },
 
@@ -79,13 +74,8 @@ let questions = [
 
   {
     question: "4. Which of the following is not a reserved word in JavaScript?",
-    choices: [
-        "A. interface", 
-        "B. throws", 
-        "C. program", 
-        "D. short"
-    ],
-    answer: "C. program", 
+    choices: ["A. interface", "B. throws", "C. program", "D. short"],
+    answer: "C. program",
   },
 
   {
@@ -125,8 +115,7 @@ let questions = [
   },
 
   {
-    question:
-      "8. How do you initialize an array in JavaScript?",
+    question: "8. How do you initialize an array in JavaScript?",
     choices: [
       "A. var Geeks= “Geek1”, “Geek2”, “Geek3”",
       "B. var Geeks=(1:Geek1, 2:Geek2, 3:Geek3)",
@@ -138,69 +127,100 @@ let questions = [
 ];
 
 function startTimer() {
-    openingEl.setAttribute("style","display:none")
+  openingEl.setAttribute("style", "display:none");
+  timeLeftEl.textContent = timeRemaining;
+  let timerInterval = setInterval(() => {
+    timeRemaining--;
     timeLeftEl.textContent = timeRemaining;
-    let timerInterval = setInterval(() => {
-        timeRemaining--;
-        timeLeftEl.textContent = timeRemaining;
 
-        if (timeRemaining === 0) {
-            clearInterval(timerInterval);
-            stopQuiz();
-            alert("Time's Up!");
-        }
-    }, 1000);
-    showNextQuestion();
+    if (timeRemaining === 0) {
+      clearInterval(timerInterval);
+      stopQuiz();
+      alert("Time's Up!");
+    }
+  }, 1000);
+  showNextQuestion();
 }
 
 function showNextQuestion() {
-    if (questionBoxEl.style.display === 'none') {
-        questionBoxEl.style.display = 'block';
-    }
-    questionsEl.textContent = questions[currentQuestion].question;
-    questionBtnA.textContent = questions[currentQuestion].choices[0];
-    questionBtnB.textContent = questions[currentQuestion].choices[1];
-    questionBtnC.textContent = questions[currentQuestion].choices[2];
-    questionBtnD.textContent = questions[currentQuestion].choices[3];
+  if (questionBoxEl.style.display === "none") {
+    questionBoxEl.style.display = "block";
+  }
+  questionsEl.textContent = questions[currentQuestion].question;
+  questionBtnA.textContent = questions[currentQuestion].choices[0];
+  questionBtnB.textContent = questions[currentQuestion].choices[1];
+  questionBtnC.textContent = questions[currentQuestion].choices[2];
+  questionBtnD.textContent = questions[currentQuestion].choices[3];
 }
 
-answersEL.addEventListener("click", function(event) {
-    let element = event.target;
-  
-    if (element.matches("button")) {
-      element = element.textContent;
-    }
-    if (element == questions[currentQuestion].answer) {
-      rightWrongEl.textContent = "That's Right!";
-      rightAnswers++;
-    }
-    else {
-      rightWrongEl.textContent = "Wrong! 10 Second Penalty!";
-      timeRemaining = timeRemaining - 10;
-      timeLeftEl.textContent = timeRemaining;
-      wrongAnswers++;
-    }
-    if (currentQuestion < questions.length - 1) {
-      currentQuestion++;
-      showNextQuestion();
-    }
-    else {
-      clearInterval(timeRemaining);
-      stopQuiz();
-      fScoreEl.textContent = rightAnswers
-    }
-})
+answersEL.addEventListener("click", function (event) {
+  let element = event.target;
+
+  if (element.matches("button")) {
+    element = element.textContent;
+  }
+  if (element == questions[currentQuestion].answer) {
+    rightWrongEl.textContent = "That's Right!";
+    rightAnswers++;
+  } else {
+    rightWrongEl.textContent = "Wrong! 10 Second Penalty!";
+    timeRemaining = timeRemaining - 10;
+    timeLeftEl.textContent = timeRemaining;
+    wrongAnswers++;
+  }
+  if (currentQuestion < questions.length - 1) {
+    currentQuestion++;
+    showNextQuestion();
+  } else {
+    //Not working!! Need to figure out how to access timeInterval from here
+    clearInterval(timeRemaining);
+    stopQuiz();
+    fScoreEl.textContent = rightAnswers;
+  }
+});
 
 function stopQuiz() {
-  questionBoxEl.style.display = 'none'
-  finalEl.style.display = 'block'
+  questionBoxEl.style.display = "none";
+  finalEl.style.display = "block";
 }
 
-startBtn.addEventListener("click", function(event) {
-    startTimer();
-})
+let quizResult = {};
+let highScores = [];
 
+// init();
 
+// function renderScores() {
+//   scoreList.innerHTML = "";
+
+//   for (var i = 0; i < highScores.length; i++) {
+//     let highScore = highScores[i];
+
+//     let li = document.createElement("li");
+//     li.textContent = highScore;
+//     li.setAttribute("data-index", i);
+
+//     scoreList.appendChild(li);
+//   }
+// }
+
+submitBtn.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let initials = initialsEl.value.trim();
+  let finalScore = rightAnswers;
+
+  if (initials === "") {
+    return;
+  }
+
+  quizResult = { player: initials, score: finalScore };
+  highscoreList.push(quizResult);
+  initialsEl.value = "";
+});
+
+startBtn.addEventListener("click", function (event) {
+  startTimer();
+});
 
 // quiz questions obtained from https://www.geeksforgeeks.org/javascript-quiz-set-1/?ref=lbp and https://www.geeksforgeeks.org/javascript-quiz-set-2/?ref=lbp
 
